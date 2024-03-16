330a39b8e3f6107238a32a46c9415869b77d4d37
Markayo21 <159958226+Markayo21@users.noreply.github.com>
2024-03-16 23:05:20 +0300

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,36 @@
+void main() {
+
+  String name = "Marko Wainaina";
+  String residence = " Kenya" ;
+
+  print( "My name is $name and i live in $residence");
+  }
+
+  void main() {
+    bool issingle = true;
+    print("Marital status : $isssingle");
+  }
+
+  void main(){
+    List<String>names = [ "john","Peter","Agnes"];
+    print("The names of my siblings are $names");
+    print("one of my siblings[0] is called ${names[0]}");
+    print("one of my siblings [1] is called ${names[1]}");
+    print("one of my siblings [2] is called ${names[2]}")
+
+
+    print(names);
+  }
+
+void main(){
+  Map<String, int> ages ={'john': 27,'Peter': 26,'Agnes': 20};
+  print("My siblings are aged : $ages ");
+}
+
+void main() {
+int num1 = 50;
+double num2 = 1234.5;
+
+print("i work for $num1 hours in a week");
+print("I earn roughly ksh $num2 in a day");
+}
