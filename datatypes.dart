eeef6973681ea2f661c86fb9bd8eb2d4d925f38f
Markayo21 <159958226+Markayo21@users.noreply.github.com>
2024-03-25 20:17:18 +0300

    Update datatypes.dart

--- datatypes.dart
+++ datatypes.dart
@@ -4,33 +4,24 @@ void main() {
   String residence = " Kenya" ;
 
   print( "My name is $name and i live in $residence");
-  }
+  
+  bool single = true;
+  print("Are you single? : $single");
+  
+  List<String>names = [ "john","Peter","Agnes"];
+  print("The names of my siblings are $names");
+  print("one of my siblings[0] is called ${names[0]}");
+  print("one of my siblings [1] is called ${names[1]}");
+  print("one of my siblings [2] is called ${names[2]}");
 
-  void main() {
-    bool issingle = true;
-    print("Marital status : $isssingle");
-  }
-
-  void main(){
-    List<String>names = [ "john","Peter","Agnes"];
-    print("The names of my siblings are $names");
-    print("one of my siblings[0] is called ${names[0]}");
-    print("one of my siblings [1] is called ${names[1]}");
-    print("one of my siblings [2] is called ${names[2]}")
-
-
-    print(names);
-  }
-
-void main(){
+  print(names);
+  
   Map<String, int> ages ={'john': 27,'Peter': 26,'Agnes': 20};
   print("My siblings are aged : $ages ");
-}
 
-void main() {
-int num1 = 50;
-double num2 = 1234.5;
+  int num1 = 50;
+  double num2 = 1234.5;
 
-print("i work for $num1 hours in a week");
-print("I earn roughly ksh $num2 in a day");
+  print("i work for $num1 hours in a week");
+  print("I earn roughly ksh $num2 in a day");
 }
